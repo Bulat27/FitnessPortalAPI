df53ae08779f7474b3e08d8f8a4ef156ab3a9cda
Bulat27 <nbulat99@gmail.com>
2022-07-05 17:47:50 +0200

    Inicijalizacija projekta

---
+++ src/test/java/rs/ac/bg/fon/njt/fitnessportal/FitnessPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.njt.fitnessportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

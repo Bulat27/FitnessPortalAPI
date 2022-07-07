0655ed20580078f1edd6e44ad7029982beedeab9
Bulat27 <nbulat99@gmail.com>
2022-07-07 17:13:59 +0200

    Dodat Authorization header u listu dostupnih header-a

--- src/main/java/rs/ac/bg/fon/njt/fitnessportal/security/configuration/SecurityConfiguration.java
+++ src/main/java/rs/ac/bg/fon/njt/fitnessportal/security/configuration/SecurityConfiguration.java
@@ -42,7 +42,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors()
+                .and()
                 .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
                 .and()

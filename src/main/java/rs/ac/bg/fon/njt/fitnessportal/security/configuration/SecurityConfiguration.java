9930d90a6d4829cabf43e2903befad619d33147c
Bulat27 <nbulat99@gmail.com>
2022-07-07 13:42:28 +0200

    Dodata CORS konfiguracija

--- src/main/java/rs/ac/bg/fon/njt/fitnessportal/security/configuration/SecurityConfiguration.java
+++ src/main/java/rs/ac/bg/fon/njt/fitnessportal/security/configuration/SecurityConfiguration.java
@@ -42,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
                 .and()
